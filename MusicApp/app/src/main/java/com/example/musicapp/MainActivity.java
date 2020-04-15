4d1c60c7a72a2b6286f068b7a7eb8d8605ef67e7
Daniel Aguiar <idd_daguiar@uolinc.com>
2020-04-14 23:41:44 -0300

    Created Hello World and MusicApp

---
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.meme);
+
+        buttonPlay();
+        buttonStop();
+    }
+
+    private void buttonStop() {
+        Button buttonStop = (Button) findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+            }
+        });
+    }
+
+    private void buttonPlay() {
+        Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+    }
+}
